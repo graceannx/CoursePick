9c891cd5630c3a315039dd4f38f21697612c978f
Grace Richardson <gracerichardson2021@u.northwestern.edu>
2020-09-19 17:24:53 -0500

    First

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome 2 My app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a9bfd6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    color: '#fff',
+    fontSize: 32,
+
+  }
+  
 });
